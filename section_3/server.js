af4e287058d859ad2d6e620a9bf908eccdf10409
h1m <himanshu.yadav643@gmail.com>
2021-01-24 10:10:06 +0530

    understood about req, res model

--- section_3/server.js
+++ section_3/server.js
@@ -2,7 +2,13 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  console.log(req);
+  console.log(req.url, req.method); // with request, we can check the url, method and the headers like req.url, req.method and req.headers
+  // process.exit() // it hard exits the event loop
+  res.setHeader("Content-Type", "text/html"); // this will set the response header
+  res.write("<html>");
+  res.write("<body> <h1> Hi from the Server! </h1> </body>");
+  res.write("</html>");
+  res.end();
 }); // createServer takes requestListener as an argrument. requestListener is a function that runs/execute for each incoming requests.
 
 server.listen(5000);
