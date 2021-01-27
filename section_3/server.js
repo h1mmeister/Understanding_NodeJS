37c0113974f8a913534e519b44558586200d88d8
h1m <himanshu.yadav643@gmail.com>
2021-01-27 16:24:26 +0530

    routing requests

--- section_3/server.js
+++ section_3/server.js
@@ -1,9 +1,43 @@
 /* eslint-disable */
 const http = require("http");
+const fs = require("fs");
 
 const server = http.createServer((req, res) => {
-  console.log(req.url, req.method); // with request, we can check the url, method and the headers like req.url, req.method and req.headers
+  // console.log(req.url, req.method); // with request, we can check the url, method and the headers like req.url, req.method and req.headers
   // process.exit() // it hard exits the event loop
+  const url = req.url;
+  const method = req.method;
+
+  if (url === "/") {
+    res.write("<html>");
+    res.write("<head><title>Enter message</title></head>");
+    res.write(
+      '<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>'
+    );
+    res.write("</html>");
+    return res.end();
+  }
+
+  if (url === "/message" && method === "POST") {
+    const body = [];
+
+    req.on("data", (chunk) => {
+      console.log(chunk);
+      body.push(chunk);
+    });
+
+    req.on("end", () => {
+      const parsedBody = Buffer.concat(body).toString();
+      console.log(parsedBody);
+      const message = parsedBody.split("=")[1];
+      fs.writeFileSync("message.txt", message);
+    });
+
+    res.statusCode = 302;
+    res.setHeader("Location", "/");
+    return res.end();
+  }
+
   res.setHeader("Content-Type", "text/html"); // this will set the response header
   res.write("<html>");
   res.write("<body> <h1> Hi from the Server! </h1> </body>");
