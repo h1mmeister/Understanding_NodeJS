4e3ace2d551ba371f8cc0ad38b935cd79a96942f
h1m <himanshu.yadav643@gmail.com>
2021-01-30 07:01:04 +0530

    section 1 completed, learnt about event loop and core modules

--- section_3/server.js
+++ section_3/server.js
@@ -1,48 +1,11 @@
 /* eslint-disable */
 const http = require("http");
-const fs = require("fs");
 
-const server = http.createServer((req, res) => {
-  // console.log(req.url, req.method); // with request, we can check the url, method and the headers like req.url, req.method and req.headers
-  // process.exit() // it hard exits the event loop
-  const url = req.url;
-  const method = req.method;
+const routes = require("./routes");
 
-  if (url === "/") {
-    res.write("<html>");
-    res.write("<head><title>Enter message</title></head>");
-    res.write(
-      '<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>'
-    );
-    res.write("</html>");
-    return res.end();
-  }
-
-  if (url === "/message" && method === "POST") {
-    const body = [];
-
-    req.on("data", (chunk) => {
-      console.log(chunk);
-      body.push(chunk);
-    });
-
-    return req.on("end", () => {
-      const parsedBody = Buffer.concat(body).toString();
-      console.log(parsedBody);
-      const message = parsedBody.split("=")[1];
-      fs.writeFile("message.txt", message, (err) => {
-        res.statusCode = 302;
-        res.setHeader("Location", "/");
-        return res.end();
-      });
-    });
-  }
-
-  res.setHeader("Content-Type", "text/html"); // this will set the response header
-  res.write("<html>");
-  res.write("<body> <h1> Hi from the Server! </h1> </body>");
-  res.write("</html>");
-  res.end();
-}); // createServer takes requestListener as an argrument. requestListener is a function that runs/execute for each incoming requests.
+const server = http.createServer(routes);
+// console.log(req.url, req.method); // with request, we can check the url, method and the headers like req.url, req.method and req.headers
+// process.exit() // it hard exits the event loop
+// createServer takes requestListener as an argrument. requestListener is a function that runs/execute for each incoming requests.
 
 server.listen(5000);
