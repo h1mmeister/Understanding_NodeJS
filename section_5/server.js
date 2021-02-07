2e88d0e3cf62a18dc57cd4ebdf9f184e50b9c61c
h1m <himanshu.yadav643@gmail.com>
2021-02-07 09:34:23 +0530

    understanding middleware

--- section_5/server.js
+++ section_5/server.js
@@ -1,7 +1,20 @@
-const http = require("http");
+// const http = require("http");
 
 const express = require("express");
 
 const app = express();
-const server = http.createServer(app);
-server.listen(5000);
+
+// app.use((req, res, next) => {
+//   console.log("This is first middleware");
+//   next(); // next is also a function - allows the request to move to the next middleware
+// }); // use allows us to add new middleware functions - accepts an array of request handlers
+
+app.use("/", (req, res, next) => {
+  console.log("This is second middleware");
+  res.send("<h1> Hi, this is my website </h1>"); // this allows us to send a response back
+});
+
+// const server = http.createServer(app);
+// server.listen(5000);
+
+app.listen(5000); // this will do the same as the above two lines
