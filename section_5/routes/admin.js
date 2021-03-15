9a7a2b22ceac1e21ecef3c4e60b9ae86cf84eb06
h1m <himanshu.yadav643@gmail.com>
2021-03-16 04:43:07 +0530

    finished section 5

--- section_5/routes/admin.js
+++ section_5/routes/admin.js
@@ -1,6 +1,7 @@
-const { Router } = require("express");
 const express = require("express");
 const path = require("path");
+
+const rootDir = require("../util/path");
 
 const router = express.Router();
 
@@ -9,7 +10,7 @@ router.get("/add-product", (req, res, next) => {
   //   res.send(
   //     '<form action="/admin/add-product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>'
   //   );
-  res.sendFile(path.join(__dirname, "../", "views", "add-product.html"));
+  res.sendFile(path.join(rootDir, "views", "add-product.html"));
 });
 
 router.post("/add-product", (req, res, next) => {
--- section_5/routes/shop.js
+++ section_5/routes/shop.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const path = require("path");
 
+const rootDir = require("../util/path");
+
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
   // console.log("This is second middleware");
   // res.send("<h1> Hi, this is my website </h1>"); // this allows us to send a response back
-  res.sendFile(path.join(__dirname, "../", "views", "shop.html"));
+  res.sendFile(path.join(rootDir, "views", "shop.html"));
 });
 
 module.exports = router;
